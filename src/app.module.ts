357b6aba99da0888fe9aff913c4b5d21ab45c065
CaiqueDS <117022913+caiquesantosss@users.noreply.github.com>
2025-03-25 21:33:51 -0300

    first commit

---
+++ src/app.module.ts
@@ -0,0 +1,19 @@
+import { Module } from '@nestjs/common';
+import { PrismaService } from './prisma/prisma.service';
+import { CreateAccountController } from './controllers/create-account.controller';
+import { ConfigModule } from '@nestjs/config'
+import { envSchema } from './env';
+
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true, 
+      validate: (env) => envSchema.parse(env)
+    })
+  ],
+  controllers: [CreateAccountController],
+  providers: [
+    PrismaService],
+})
+export class AppModule {}

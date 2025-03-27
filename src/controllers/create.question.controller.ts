c13d7857188e7d00ef0385d3ece9f6e52b4fbb0f
CaiqueDS <117022913+caiquesantosss@users.noreply.github.com>
2025-03-27 01:38:02 -0300

    feat: create list question controller

--- src/controllers/create.question.controller.ts
+++ src/controllers/create.question.controller.ts
@@ -1,23 +1,55 @@
-import {
-  Controller,
-  HttpCode,
-  Post,
-  UseGuards,
-} from '@nestjs/common'
+import { Body, Controller, HttpCode, Post, UseGuards } from '@nestjs/common'
+import { ENTRY_PROVIDER_WATERMARK } from '@nestjs/common/constants'
 import { CurrentUser } from 'src/auth/current-user-decorator'
 import { JwtAuthGuard } from 'src/auth/jwt.AuthGuard'
 import { UserPayload } from 'src/auth/jwt.strategy'
+import { ZodValidationPipe } from 'src/pipes/zod-validation-pipe'
+import { PrismaService } from 'src/prisma/prisma.service'
+import { z } from 'zod'
+
+const createQuestionBodySchema = z.object({
+  title: z.string(),
+  content: z.string(),
+})
+
+const bodyValidationPipe = new ZodValidationPipe(createQuestionBodySchema)
+
+type CreateQuestionBodySchema = z.infer<typeof createQuestionBodySchema>
 
 @Controller('/questions')
 @UseGuards(JwtAuthGuard)
 export class CreateQuestionController {
-  constructor() {}
+  constructor(private prisma: PrismaService) {}
 
   @Post()
   @HttpCode(201)
-  async handle(@CurrentUser() user: UserPayload) {
-    console.log(user.sub)
+  async handle(
+    @Body(bodyValidationPipe) body: CreateQuestionBodySchema,
+    @CurrentUser() user: UserPayload
+  ) {
+    const { title, content } = body
+    const userId = user.sub
 
-    return "ok"
+    const slug = this.ConvertToSlug(title)
+
+    await this.prisma.question.create({
+      data: {
+        authorId: userId,
+        title,
+        content,
+        slug
+      },
+    })
+  }
+
+  private ConvertToSlug(title: string): string {
+    return title
+      .normalize('NFD')
+      .replace(/[\u0300-\u036f]/g, '')
+      .toLowerCase()
+      .trim()
+      .replace(/\s+/g, '-')
+      .replace(/[^a-z0-9-]/g, '')
+      .replace(/-+/g, '-')
   }
 }
---
+++ src/controllers/fetch-recent-questions.controller.ts
@@ -0,0 +1,38 @@
+import { Controller, Get, HttpCode, Query, UseGuards } from '@nestjs/common'
+import { JwtAuthGuard } from 'src/auth/jwt.AuthGuard'
+import { ZodValidationPipe } from 'src/pipes/zod-validation-pipe'
+import { PrismaService } from 'src/prisma/prisma.service'
+import { z } from 'zod'
+
+const PageQueryParamsSchema = z
+  .string()
+  .optional()
+  .default('1')
+  .transform(Number)
+  .pipe(z.number().min(1))
+
+const QueryValidationPipe = new ZodValidationPipe(PageQueryParamsSchema)
+
+type PageParamsSchema = z.infer<typeof PageQueryParamsSchema>
+
+@Controller('/questions')
+@UseGuards(JwtAuthGuard)
+export class FetchRecentQuestionsController {
+  constructor(private prisma: PrismaService) {}
+
+  @Get()
+  @HttpCode(201)
+  async handle(@Query('page', QueryValidationPipe) page: PageParamsSchema) {
+    const perPage = 1
+
+    const questions = await this.prisma.question.findMany({
+      take: perPage, 
+      skip: (page - 1) * perPage,  
+      orderBy: {
+        createdAt: 'desc',
+      },
+    })
+
+    return { questions }
+  }
+}

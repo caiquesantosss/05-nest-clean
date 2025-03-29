b571fbf32a2357a0f6ffa3991ead11a8108bd916
CaiqueDS <117022913+caiquesantosss@users.noreply.github.com>
2025-03-28 22:20:06 -0300

    feat: mappers

---
+++ src/infra/database/prisma/mappers/prisma-question-mapper.ts
@@ -0,0 +1,20 @@
+import { UniqueEntityId } from "@/core/entities/unique-entity";
+import { Question } from "@/domain/forum/enterprise/entities/question";
+import { Slug } from "@/domain/forum/enterprise/entities/values-object/slug";
+import { Question as PrismaQuestion } from "@prisma/client";
+
+export class PrismaQuestionMapper {
+    static toDomain(raw: PrismaQuestion): Question {
+        return Question.create({
+            title: raw.title, 
+            content: raw.content, 
+            authorId: new UniqueEntityId(raw.authorId),
+            bestAnswerId: undefined,
+            slug: Slug.create(raw.slug),
+            createdAt: raw.createdAt,
+            updatedAt: raw.updateAt 
+
+        }, new UniqueEntityId(raw.id))
+
+    }
+}
--- src/infra/database/prisma/repositories/prisma-question-repository.ts
+++ src/infra/database/prisma/repositories/prisma-question-repository.ts
@@ -2,9 +2,13 @@ import { PaginationParams } from '@/core/repositories/pagenations-params'
 import { QuestionRepository } from '@/domain/forum/application/repositories/question-repository'
 import { Question } from '@/domain/forum/enterprise/entities/question'
 import { Injectable } from '@nestjs/common'
+import { PrismaService } from '../prisma.service'
+import { PrismaQuestionMapper } from '../mappers/prisma-question-mapper'
 
 @Injectable()
 export class PrismaQuestionsRepository implements QuestionRepository {
+    constructor(private prisma: PrismaService) {}
+
   create(question: Question): Promise<void> {
     throw new Error('Method not implemented.')
   }
@@ -20,7 +24,17 @@ export class PrismaQuestionsRepository implements QuestionRepository {
   findBySlug(slug: string): Promise<Question | null> {
     throw new Error('Method not implemented.')
   }
-  findById(id: string): Promise<Question | null> {
-    throw new Error('Method not implemented.')
+  async findById(id: string): Promise<Question | null> {
+    const question = await this.prisma.question.findUnique({
+        where: {
+            id,
+        },
+    })
+
+    if (!question) {
+        return null
+    }
+
+    return PrismaQuestionMapper.toDomain(question)
   }
 }
